3ef39c0ac61eac87495fef3ddff28931cfa02966
coxde <63153334+coxde@users.noreply.github.com>
2025-04-20 12:36:15 +0100

    fix(script): fix clean temp files

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -3,10 +3,12 @@
 set -ouex pipefail
 
 # Clean temp files
-# Enable extended globbing
-shopt -s extglob
-
+# https://github.com/ublue-os/bazzite/blob/main/build_files/finalize
 dnf5 clean all
+
 rm -rf /tmp/* || true
-rm -rf /var/!(cache)
-rm -rf /var/cache/!(rpm-ostree)
+find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
+find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
+
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
