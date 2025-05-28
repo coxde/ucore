0f30d06f5b5acc8b8e6c24147e2cf396719bffc1
coxde <63153334+coxde@users.noreply.github.com>
2025-05-29 00:57:44 +0100

    feat: install eza manually

--- build_files/01-packages.sh
+++ build_files/01-packages.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-# Install packages (TODO: add eza when F42 pkg available)
+# Install packages
 dnf5 -y install \
     btop \
     cockpit \
@@ -18,3 +18,13 @@ dnf5 -y install \
     restic \
     ugrep \
     zoxide
+
+# Install 3rd party packages
+## eza
+curl --retry 3 -L -s https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz | tar xz
+install -Dm0755 -t /usr/local/bin/ eza
+rm -f eza
+
+curl --retry 3 --create-dirs -Lo \
+    /usr/share/fish/vendor_completions.d/eza.fish \
+    https://github.com/eza-community/eza/raw/refs/heads/main/completions/fish/eza.fish
