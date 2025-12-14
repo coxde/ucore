0f3e4f476e8ad1d463a5d557dea6c8a72a9407a2
coxde <63153334+coxde@users.noreply.github.com>
2025-12-14 17:10:30 +0000

    feat: refactor clean

--- Containerfile
+++ Containerfile
@@ -18,8 +18,7 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
-    /ctx/build_files/build.sh && \
-    ostree container commit
+    /ctx/build_files/build.sh
 
 # Linting
-# RUN bootc container lint
+RUN bootc container lint
--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-set -ouex pipefail
+set -eoux pipefail
 
-# Clean temp files
-# https://github.com/ublue-os/bazzite/blob/main/build_files/finalize
-dnf5 clean all
-
-rm -rf /tmp/* || true
+find /boot/ -maxdepth 1 -mindepth 1 -exec rm -fr {} \; || true
+find /tmp/* -maxdepth 0 -type d \! -name rpms -exec rm -fr {} \; || true
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
 
-mkdir -p /var/tmp
-chmod -R 1777 /var/tmp
+mkdir -p /var/tmp \
+&& chmod -R 1777 /var/tmp
